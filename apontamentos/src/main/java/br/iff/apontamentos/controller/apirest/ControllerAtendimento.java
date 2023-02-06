574ae3d0384c89b1a5053239f8710b64898da78e
GleysonRibeiro <89668176+GleysonRibeiro@users.noreply.github.com>
2023-02-06 20:37:37 -0300

    Atualizando projeto

--- apontamentos/src/main/java/br/iff/apontamentos/controller/apirest/ControllerAtendimento.java
+++ apontamentos/src/main/java/br/iff/apontamentos/controller/apirest/ControllerAtendimento.java
@@ -1,8 +1,10 @@
 package br.iff.apontamentos.controller.apirest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,14 @@ public class ControllerAtendimento {
 	@GetMapping("/{id}")
     public String page(@PathVariable("id") int id) {
 		return "Olá Mundo" + id;
+	}
+	
+	@PostMapping("/new")
+	public String newAtendimento(
+			@RequestParam(name = "nome") String nome,
+			@RequestParam(name = "sobrenome") String sobrenome			
+	) {
+		return "Parametros:" + nome + sobrenome;
 	}
 
 }
