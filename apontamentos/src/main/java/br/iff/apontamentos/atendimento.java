75687866d2b4f608e53672c9c65f2125d15c91de
GleysonRibeiro <89668176+GleysonRibeiro@users.noreply.github.com>
2023-02-06 21:03:28 -0300

    atualizando classes de negocio

--- apontamentos/src/main/java/br/iff/apontamentos/atendimento.java
+++ apontamentos/src/main/java/br/iff/apontamentos/atendimento.java
@@ -10,5 +10,55 @@ public class atendimento {
 	private int qtdPassageiros;
 	private int odometroIni;
 	private int odometroFim;
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public equipamento getEquipamento() {
+		return equipamento;
+	}
+	public void setEquipamento(equipamento equipamento) {
+		this.equipamento = equipamento;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public int getKmRodado() {
+		return kmRodado;
+	}
+	public void setKmRodado(int kmRodado) {
+		this.kmRodado = kmRodado;
+	}
+	public double getHoraExtra() {
+		return horaExtra;
+	}
+	public void setHoraExtra(double horaExtra) {
+		this.horaExtra = horaExtra;
+	}
+	public int getQtdPassageiros() {
+		return qtdPassageiros;
+	}
+	public void setQtdPassageiros(int qtdPassageiros) {
+		this.qtdPassageiros = qtdPassageiros;
+	}
+	public int getOdometroIni() {
+		return odometroIni;
+	}
+	public void setOdometroIni(int odometroIni) {
+		this.odometroIni = odometroIni;
+	}
+	public int getOdometroFim() {
+		return odometroFim;
+	}
+	public void setOdometroFim(int odometroFim) {
+		this.odometroFim = odometroFim;
+	}
+	
+	
 
 }
--- apontamentos/src/main/java/br/iff/apontamentos/equipamento.java
+++ apontamentos/src/main/java/br/iff/apontamentos/equipamento.java
@@ -7,5 +7,36 @@ public class equipamento {
 	private int regime;
 	private String tipo;
 	private String area;
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public veiculo getVeiculo() {
+		return veiculo;
+	}
+	public void setVeiculo(veiculo veiculo) {
+		this.veiculo = veiculo;
+	}
+	public int getRegime() {
+		return regime;
+	}
+	public void setRegime(int regime) {
+		this.regime = regime;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
 	
 }
