1a38ee01fb5d5511d050307a44a01fa5f1f4255b
GleysonRibeiro <89668176+GleysonRibeiro@users.noreply.github.com>
2023-02-13 20:09:55 -0300

    Atualizando controladores
    Inseridos mapeamento para PUT e DELETE

--- apontamentos/src/main/java/br/iff/apontamentos/controller/apirest/ControllerEquipamento.java
+++ apontamentos/src/main/java/br/iff/apontamentos/controller/apirest/ControllerEquipamento.java
@@ -1,14 +1,17 @@
 package br.iff.apontamentos.controller.apirest;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import br.iff.apontamentos.equipamento;
+import br.iff.apontamentos.veiculo;
 
 @RestController
 @RequestMapping(path = "/apirest/equipamento")
@@ -28,6 +31,22 @@ public class ControllerEquipamento {
 		equipamento novoEquipamento = new equipamento(numero);
 		
 		return "Equipamento:" + numero;
+	}
+	
+	@PutMapping
+	public String atualizarEquipamento(
+			@RequestParam(name = "numero") int numero,
+			@RequestParam(name = "veiculo") veiculo veiculo, 
+			@RequestParam(name= "regime") int regime,
+			@RequestParam(name= "tipo") String tipo,
+			@RequestParam(name= "area") String area){
+		
+		return numero + regime + tipo + area;		
+	}
+	
+	@DeleteMapping
+	public String deletarEquipamento(@RequestParam(name="numero") int numero) {
+		return "Equipamento deletado:" + numero;
 	}
 
 }
--- apontamentos/src/main/java/br/iff/apontamentos/controller/apirest/ControllerVeiculo.java
+++ apontamentos/src/main/java/br/iff/apontamentos/controller/apirest/ControllerVeiculo.java
@@ -1,15 +1,17 @@
 package br.iff.apontamentos.controller.apirest;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.iff.apontamentos.equipamento;
+
 import br.iff.apontamentos.veiculo;
 
 @RestController
@@ -26,10 +28,27 @@ public class ControllerVeiculo {
 	public String newVeiculo(
 			@RequestParam(name = "prefixo") int prefixo)
 	{
+		@SuppressWarnings("unused")
 		veiculo novoVeiculo = new veiculo(prefixo);
 		
 		return "Veiculo:" + prefixo;
 	}
+	
+	@PutMapping
+	public String atualizarVeiculo(
+			@RequestParam(name = "prefixo") int prefixo,
+			@RequestParam(name = "placa") String placa, 
+			@RequestParam(name= "modelo") String modelo,
+			@RequestParam(name= "odometro") int odometro){
+		
+		return prefixo + placa + modelo + odometro;		
+	}
+	
+	@DeleteMapping
+	public String deletarVeiculo(@RequestParam(name="prefixo") int prefixo) {
+		return "Veículo deletado:" + prefixo;
+	}
+	
 
 	
 }
