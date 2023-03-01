1d2667c682bd838db0ce16b8073c4ef1561e14a3
Gleyson Ribeiro <89668176+GleysonRibeiro@users.noreply.github.com>
2023-03-01 00:17:47 +0000

    Atualizando classe Veiculo

---
+++ apontamentos/src/main/java/br/iff/apontamentos/Veiculo.java
@@ -0,0 +1,37 @@
+package br.iff.apontamentos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Veiculo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@NotNull(message = "Prefixo obrigatório")
+	private int prefixo;
+  @NotNull(message = "Placa obrigatória")
+	private String placa;
+  @NotNull(message = "Modelo obrigatório")
+	private String modelo;
+	
+	public Veiculo (int prefixo, String placa, String modelo) {
+		this.prefixo = prefixo;
+    this.placa=placa;
+    this.modelo=modelo;
+	}
+
+  public void alterarCadastro (String placa, String modelo){
+    this.placa=placa;
+    this.modelo=modelo;
+  }
+	
+	
+	
+	
+
+}
